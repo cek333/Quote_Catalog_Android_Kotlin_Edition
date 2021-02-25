c8dd2a83702d82fea60a4bce52b1545d30fe5721
Cecil <69444825+cek333@users.noreply.github.com>
2021-02-25 12:52:16 -0500

    add code to request/check permissions

--- app/src/main/java/com/example/android/quote_catalog/MainActivity.kt
+++ app/src/main/java/com/example/android/quote_catalog/MainActivity.kt
@@ -3,14 +3,41 @@ package com.example.android.quote_catalog
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var requestPermissionLauncher : ActivityResultLauncher<Array<String>>
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     setSupportActionBar(findViewById(R.id.toolbar))
+
+    // Register the permissions callback, which handles the user's response to the
+    // system permissions dialog.
+    requestPermissionLauncher =
+      registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+          isGranted: Map<String, Boolean> ->
+            if (isGranted.keys.all { permission -> isGranted[permission] == true }) {
+              // Permission is granted.
+              Toast.makeText(
+                this,
+                getString(R.string.permissions_success),
+                Toast.LENGTH_LONG
+              ).show()
+            } else {
+              // Explain to the user that the feature is unavailable
+              Toast.makeText(
+                this,
+                getString(R.string.permissions_warning),
+                Toast.LENGTH_LONG
+              ).show()
+            }
+      }
   }
 
   override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -24,8 +51,29 @@ class MainActivity : AppCompatActivity() {
     // automatically handle clicks on the Home/Up button, so long
     // as you specify a parent activity in AndroidManifest.xml.
     return when (item.itemId) {
-      R.id.menu_enable_permissions -> true
+      R.id.menu_enable_permissions -> {
+        requestPermissionsIfNecessary()
+        true
+      }
       else -> super.onOptionsItemSelected(item)
     }
   }
+
+  private fun requestPermissionsIfNecessary() {
+    when {
+      checkAllPermissions(this) -> {
+        Toast.makeText(
+          this,
+          getString(R.string.permissions_confirmation),
+          Toast.LENGTH_LONG
+        ).show()
+      }
+      else -> {
+        // You can directly ask for the permission.
+        // The registered ActivityResultCallback gets the result of this request.
+        requestPermissionLauncher.launch(PERMISSIONS)
+      }
+    }
+  }
+
 }
--- app/src/main/java/com/example/android/quote_catalog/Util.kt
+++ app/src/main/java/com/example/android/quote_catalog/Util.kt
@@ -1,5 +1,13 @@
 package com.example.android.quote_catalog
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat
+
 // Default colours
 const val DEFAULT_BG_COLOR = -1 // white
 const val DEFAULT_TXT_COLOR = -16777216 // black
@@ -11,3 +19,27 @@ const val BUNDLE_QUOTE_TXT = "BUNDLE_QUOTE_TXT"
 
 // Other constants
 const val MAX_FILENAME_LEN = 75
+
+// Shared Permissions Checking Code
+val PERMISSIONS = arrayOf(
+  Manifest.permission.READ_EXTERNAL_STORAGE,
+  Manifest.permission.WRITE_EXTERNAL_STORAGE
+)
+
+fun checkAllPermissions(context: Context): Boolean {
+  var hasPermissions = true
+  for (permission in PERMISSIONS) {
+    hasPermissions = hasPermissions and isPermissionGranted(context, permission)
+  }
+  return hasPermissions
+}
+
+private fun isPermissionGranted(context: Context, permission: String) =
+  ContextCompat.checkSelfPermission(context, permission) ==
+      PackageManager.PERMISSION_GRANTED
+
+// Shared utility function
+fun hideKeyboardFrom(context : Context, view: View) {
+  val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+  imm.hideSoftInputFromWindow(view.windowToken, 0);
+}
